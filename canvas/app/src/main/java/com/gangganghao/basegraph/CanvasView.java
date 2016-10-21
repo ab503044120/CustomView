ee0624d47b748b3d56e681e26e7bdf15241fa966
ab503044120 <503044120@qq.com>
2016-10-21 11:34:42 +0800

    canvas
    canvas绘制

---
+++ canvas/app/src/main/java/com/gangganghao/basegraph/CanvasView.java
@@ -0,0 +1,24 @@
+package com.gangganghao.basegraph;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * User: Administrator
+ * Date: 2016-10-21 {HOUR}:05
+ */
+public class CanvasView extends View {
+    public CanvasView(Context context) {
+        this(context, null);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        
+    }
+}  
